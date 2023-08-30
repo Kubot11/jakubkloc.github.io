98ac582d90ebe1693db9ce15494d4fe0dc14c6b9
Jakub Kloc <kubot.kk@gmail.com>
2023-08-30 18:35:57 +0200

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jakubkloc.github.io",
+  base: "/jakubkloc.github.io/",
   plugins: [react()],
 });
